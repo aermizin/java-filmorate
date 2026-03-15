70dfb44af19d91420356dc486b2dfcdb78685a55
Artem <a.ermizin.corp@yandex.ru>
2026-03-15 19:29:28 +0700

    Удалил файл checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
