b59145f5fba8d3e5426a0fdec7426e57af711afc
Artem <a.ermizin.corp@yandex.ru>
2026-04-07 23:59:13 +0700

    Фикс №13 checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -4,6 +4,7 @@ public class FilmNotFoundException extends NotFoundException {
     public FilmNotFoundException(String message) {
         super(message);
     }
+
     public FilmNotFoundException(Long filmId) {
         super("Фильм с ID " + filmId + " не найден");
     }
