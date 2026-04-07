39020b5595f44dc9f1b00ca7b1d76500f61b0da5
Artem <a.ermizin.corp@yandex.ru>
2026-04-08 01:04:54 +0700

    Фикс №18 checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,9 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -18,7 +17,6 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Optional;
 
-@AutoConfigureTestDatabase
 @SpringBootTest
 class UserControllerTest {
 
@@ -28,7 +26,7 @@ class UserControllerTest {
     @Autowired
     private UserStorage userStorage;
 
-    @AfterEach
+    @BeforeEach
     void setUp() {
         userStorage.deleteTable();
     }
