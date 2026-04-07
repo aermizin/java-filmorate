16f8cfbc57fb1ba47786f4a6450eb5a93a1b01aa
Artem <a.ermizin.corp@yandex.ru>
2026-04-08 00:08:36 +0700

    Фикс №17 checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Optional;
-@Component
+
 public interface UserStorage extends BaseStorage<User> {
 
     Optional<User> findByEmail(String email);
